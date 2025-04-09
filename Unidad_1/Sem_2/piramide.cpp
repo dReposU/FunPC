bdb52a6a7074abd7618d16e8716eee7bb1258a75
daniehuuu <148598226+daniehuuu@users.noreply.github.com>
2025-04-09 17:28:46 -0500

    Add piramide

---
+++ Unidad_1/Sem_2/piramide.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void foo(int n) {
+    if (n > 0){
+        foo(n - 1);
+    }
+    for (int i = 1; i <= n; i++){
+        cout << i;
+    }
+    
+    cout<<endl;
+}
+
+int main() {
+    int num;
+    cout << "Introduce un numero: ";
+    cin >> num;
+
+    foo(num);
+    return 0;
+}
+
