5fdd9230251b0e222c110b4f6935fd2eeefb3837
daniehuuu <148598226+daniehuuu@users.noreply.github.com>
2025-04-09 17:32:21 -0500

    Fix piramide

--- Unidad_1/Sem_2/piramide.cpp
+++ Unidad_1/Sem_2/piramide.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 void foo(int n) {
-    if (n > 0){
+    if (n > 1){
         foo(n - 1);
     }
     for (int i = 1; i <= n; i++){
         cout << i;
     }
-    
     cout<<endl;
 }
 
